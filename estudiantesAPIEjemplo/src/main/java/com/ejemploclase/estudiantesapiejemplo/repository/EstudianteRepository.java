a6246e015881a82de7d28c43941bd3324d5efd26
Hijo De Algo <tomatejuan@gmail.com>
2023-02-01 20:41:36 -0600

    Update EstudianteRepository.java

--- estudiantesAPIEjemplo/src/main/java/com/ejemploclase/estudiantesapiejemplo/repository/EstudianteRepository.java
+++ estudiantesAPIEjemplo/src/main/java/com/ejemploclase/estudiantesapiejemplo/repository/EstudianteRepository.java
@@ -9,15 +9,46 @@ public class EstudianteRepository {
 
     ArrayList<Estudiante> listaEstudiantes = new ArrayList<>();
 
+    public ArrayList<Estudiante> listarEstudiantes()
+    {
+        // Libro libro = new Libro(1,"Mi Libro","Mauricio Camacho","2020","UCenfotec");
+        // listaLibros.add(libro);
+        return listaEstudiantes;
+    }
+
+    public void agregarEstudiante(Estudiante estudiante)
+    {
+        listaEstudiantes.add(estudiante);
+    }
 
 
+    public void modificarEstudiante(Estudiante estudiante)
+    {
+        for(int i=0; i<listaEstudiantes.size();i++)
+        {
+            if(listaEstudiantes.get(i).getId()== estudiante.getId())
+            {
+                listaEstudiantes.get(i).setNombre(estudiante.getNombre());
+                listaEstudiantes.get(i).setApellidos(estudiante.getApellidos());
+                listaEstudiantes.get(i).setEdad(estudiante.getEdad());
+                listaEstudiantes.get(i).setDireccion(estudiante.getDireccion());
+
+            }
+        }
+    }
 
 
-
-
-
-
-
+    public void borrarEstudiante(int id)
+    {
+        for(Estudiante estudiante: listaEstudiantes)
+        {
+            if(estudiante.getId()==id)
+            {
+                listaEstudiantes.remove(estudiante);
+                break;
+            }
+        }
+    }
 
 
 }
