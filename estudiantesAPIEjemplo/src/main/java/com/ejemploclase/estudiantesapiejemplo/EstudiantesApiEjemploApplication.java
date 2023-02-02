04baac276a8e4857332d18fb1fd9d9b00d0b7138
Hijo De Algo <tomatejuan@gmail.com>
2023-02-01 20:11:42 -0600

    Base Spring Boot

---
+++ estudiantesAPIEjemplo/src/main/java/com/ejemploclase/estudiantesapiejemplo/EstudiantesApiEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.ejemploclase.estudiantesapiejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesApiEjemploApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudiantesApiEjemploApplication.class, args);
+    }
+
+}
---
+++ estudiantesAPIEjemplo/src/test/java/com/ejemploclase/estudiantesapiejemplo/EstudiantesApiEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemploclase.estudiantesapiejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApiEjemploApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
